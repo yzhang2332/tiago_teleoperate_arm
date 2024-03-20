7011a27f019f0a8034c2f365b8558d77258c694c
yzhang2332 <zy940185346@gmail.com>
2024-03-20 09:31:28 +0000

    rosbag ready

--- arm_teleop_keyboard/script/record_rosbag.py
+++ arm_teleop_keyboard/script/record_rosbag.py
@@ -1,26 +1,34 @@
 import subprocess
 import datetime
+import os
+import time
+import signal
+
+# Ensure the directory exists
+directory = "/home/pal/rosbags/"
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 # Format the current date and time as a string
 current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-print(current_time)
+
 
 # Define the bag file name using the current date and time
-bag_file_name = f"/home/rosbags/record_{current_time}.bag"
+# bag_file_name = f"/home/rosbags/record_{current_time}.bag"
+bag_file_name = f"{directory}record_{current_time}.bag"
+
 topics = "/xtion/rgb/image_raw /joint_states /audio"
 
 # Define the command to start recording all topics to the named bag file
-command = f"rosbag record -a -O {bag_file_name}"
+command = f"rosbag record {topics} -O {bag_file_name}"
 # Start recording
-process = subprocess.Popen(command, shell=True)
+process = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
 
 # Wait for a while or execute other logic
-# Example: sleep for 10 seconds
-import time
 time.sleep(10)
 
-# Stop recording by terminating the subprocess
-process.terminate()
+# Stop recording by sending SIGINT to the process group, emulating Ctrl+C
+os.killpg(os.getpgid(process.pid), signal.SIGINT)
 
 # Optionally, wait for the command to complete
 process.wait()
