8cfd5cd74499dd3f3256f77745de03015ca41f76
yzhang2332 <zy940185346@gmail.com>
2024-03-24 16:34:11 +1100

    compressed rosbag

--- arm_teleop_keyboard/script/record_rosbag.py
+++ arm_teleop_keyboard/script/record_rosbag.py
@@ -6,10 +6,14 @@ import signal
 
 # Define a signal handler for SIGINT
 def signal_handler(sig, frame):
-    print('Stopping recording...')
+    print('Stopping recording and throttling...')
     os.killpg(os.getpgid(process.pid), signal.SIGINT)
+    os.killpg(os.getpgid(throttle_rgb_process.pid), signal.SIGINT)
+    os.killpg(os.getpgid(audio_recording.pid), signal.SIGINT)
     process.wait()
-    print('Recording stopped.')
+    throttle_rgb_process.wait()
+    audio_recording.wait()
+    print('Recording and throttling stopped.')
     exit(0)
 
 # Register the signal handler for SIGINT (Ctrl+C)
@@ -17,6 +21,7 @@ signal.signal(signal.SIGINT, signal_handler)
 
 # Ensure the directory exists
 directory = "/home/pal/rosbags/"
+directory = "/home/yanzhang/pal/rosbags/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
@@ -28,10 +33,20 @@ current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 # bag_file_name = f"/home/rosbags/record_{current_time}.bag"
 bag_file_name = f"{directory}record_{current_time}.bag"
 
-topics = "/xtion/rgb/image_raw /joint_states /audio"
+# Throttle command for the camera topic
+throttle_rgb_command = "rosrun topic_tools throttle messages /xtion/rgb/image_raw 4.0 /xtion/rgb/image_raw_throttled"
+throttle_rgb_process = subprocess.Popen(throttle_rgb_command, shell=True, preexec_fn=os.setsid)
+
+# Start audio recording
+audio_command = "rosrun audio_capture audio_capture _device:=hw:1,0"
+audio_recording = subprocess.Popen(audio_command, shell=True, preexec_fn=os.setsid)
+
+
+# Adjusted topics with throttled camera topic
+topics = "/xtion/rgb/image_raw_throttled /joint_states /audio"
 
 # Define the command to start recording all topics to the named bag file
-command = f"rosbag record {topics} -O {bag_file_name}"
+command = f"rosbag record --lz4 {topics} -O {bag_file_name}"
 # Start recording
 process = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
 
